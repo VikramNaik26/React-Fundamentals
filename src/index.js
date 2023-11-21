5de612c336ef8e252efc077a3e7fc49fbe7f5cb5
Vikram Naik <26vikramnaik@gmail.com>
2023-11-22 00:27:12 +0530

    react with css

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+import './index.css'
+
 // const Greeting = () => <h2 className="firstClass">My First Component</h2>
 
 // jsx way of creating a component
 // const Greeting = () => React.createElement('h2', {}, 'Hello world')
 
-const Greeting = () =>
-  React.createElement('div', {}, React.createElement('h2', {}, 'Hello world'))
+/* const Greeting = () =>
+  React.createElement('div', {}, React.createElement('h2', {}, 'Hello world')) */
 
 // rather calling createElement to nest any tags we could use jsx
 
@@ -17,6 +19,38 @@ const Greeting = () =>
   </div>
 )
  */
+
+const BookList = () => {
+  return (
+    <section className='booklist'>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  )
+}
+
+const Book = () => {
+  return (
+    <article className='book'>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  )
+}
+
+const Image = () => (
+  <img
+    src="https://images-na.ssl-images-amazon.com/images/I/91n7p-j5aqL._AC_UL600_SR600,400_.jpg"
+    alt="Fourth Wing"
+  />
+)
+const Title = () => <h2>Fourth Wing (The Empyrean, 1)</h2>
+const Author = () => <h4>Rebecca Yarror</h4>
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-root.render(<Greeting />)
+// root.render(<Greeting />)
+root.render(<BookList />)
