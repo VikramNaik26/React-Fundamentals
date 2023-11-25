9d810d2a418bf9e0d5a47c4d61a491d86967201e
Vikram Naik <26vikramnaik@gmail.com>
2023-11-25 12:37:11 +0530

    src local images

--- src/books.js
+++ src/books.js
@@ -1,15 +1,20 @@
+import img1 from './images/book1.jpg'
+import img2 from './images/book2.jpg'
+import img3 from './images/book3.jpg'
+import img4 from './images/book4.webp'
+
 export const books = [
   {
     author: 'Rebecca Yarror',
     title: 'Fourth Wing (The Empyrean, 1)',
-    img: 'https://images-na.ssl-images-amazon.com/images/I/91n7p-j5aqL._AC_UL600_SR600,400_.jpg',
+    img: img1,
     id: 1,
   },
 
   {
     author: 'Mathew Perry',
     title: 'Friends, Lovers, and the Big Terrible Thing: A Memoir',
-    img: 'https://images-na.ssl-images-amazon.com/images/I/81tdvyI0MeL._AC_UL600_SR600,400_.jpg',
+    img: img2,
     id: 2,
   },
 
@@ -17,7 +22,7 @@ export const books = [
     author: 'Suzanne Collins',
     title:
       'The Ballad of Songbirds and Snakes (A Hunger Games Novel) (The Hunger Games)',
-    img: 'https://images-na.ssl-images-amazon.com/images/I/61k7JqSWOUL._AC_UL600_SR600,400_.jpg',
+    img: img3,
     id: 3,
   },
 
@@ -25,7 +30,7 @@ export const books = [
     author: 'James Clear',
     title:
       'Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones',
-    img: 'https://images-na.ssl-images-amazon.com/images/I/812eE1lO0dL._AC_UL600_SR600,400_.jpg',
+    img: img4,
     id: 4,
   },
 ]
