b8196aaac3838ef3dd3515ac290a7673ed897add
Vikram Naik <26vikramnaik@gmail.com>
2023-11-26 23:15:41 +0530

    completed react fundamentals

--- src/books.js
+++ src/books.js
@@ -1,7 +1,7 @@
 import img1 from './images/book1.jpg'
 import img2 from './images/book2.jpg'
 import img3 from './images/book3.jpg'
-import img4 from './images/book4.webp'
+import img4 from './images/book4.jpg'
 
 export const books = [
   {
